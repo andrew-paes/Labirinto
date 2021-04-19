ce681b92a7b244fff9d9ffba8b482ca17c5d1c6c
Andrew <andrew.paes@gmail.com>
2021-04-19 18:24:19 -0300

    Implementação de validação da saída

--- src/Labirinto.java
+++ src/Labirinto.java
@@ -1,15 +1,16 @@
 /*
-O labirinto será um array bidimensional de caracteres.
-No labirinto, posições com 'X' indicam que não é possível passar, enquanto posições com ' ' (espaço) indicam que é possível a passagem. 
-O local de destino está marcado no labirinto com a letra 'D'.
-O ponto inicial sempre será a posição [0][0] do array.
+OK - O labirinto será um array bidimensional de caracteres.
+OK - No labirinto, posições com 'X' indicam que não é possível passar, enquanto posições com ' ' (espaço) indicam que é possível a passagem. 
+OK - O local de destino está marcado no labirinto com a letra 'D'.
+OK - O ponto inicial sempre será a posição [0][0] do array.
 
-Criar um método recursivo (com métodos público e privado) chamado labirinto, 
-	que recebe um array de caracteres 
-	e retorna verdadeiro caso exista pelo menos um caminho para chegar ao destino 
-	e falso caso não exista.
+OK - Criar um método recursivo 
+	NO (com métodos público e privado) chamado labirinto, 
+	NO - que recebe um array de caracteres 
+	OK - e retorna verdadeiro caso exista pelo menos um caminho para chegar ao destino 
+	OK - e falso caso não exista.
 	 
-No método público, para criar o array, você deve chamar o método carregaLabirinto, criado na parte 1.
+NO - No método público, para criar o array, você deve chamar o método carregaLabirinto, criado na parte 1.
 */
 public class Labirinto {
 	private char[][] _labirinto;
@@ -25,24 +26,51 @@ public class Labirinto {
 	public void set_labirinto(char[][] _labirinto) {
 		this._labirinto = _labirinto;
 	}
-	
-	public void imprime() {
-    	System.out.println("====================");
-    	
-    	/*for(int i=0; i < _tabuleiro.length; i++) {
-    		for(int j=0; j < _tabuleiro[i].length; j++) {
-    			String jogadaAnterior = _tabuleiro[i][j];
-    			
-    			System.out.print(((jogadaAnterior != null && !jogadaAnterior.trim().isEmpty()) ? jogadaAnterior : "-")  + "\t");
-    		}
-    		
-    		System.out.println("");
-    	}*/
-    	
-    	System.out.println("====================");
-    }
 
-	public boolean temSaida() {
-		return false;
+	public void imprime() {
+		System.out.println("++++++++++++++++++++");
+		System.out.println("+ LABIRINTO");
+		System.out.println("++++++++++++++++++++");
+
+		for (int i = 0; i < _labirinto.length; i++) {
+			for (int j = 0; j < _labirinto[i].length; j++) {
+				System.out.print(_labirinto[i][j]);
+			}
+
+			System.out.println("");
+		}
+	}
+
+	public boolean temSaida(int linha, int coluna) {
+		boolean saida = false;
+
+		if (linha >= 0 && linha < _labirinto.length && coluna >= 0 && coluna < _labirinto[0].length) {
+			if (_labirinto[linha][coluna] != 'X' && _labirinto[linha][coluna] != '.') {
+				if (_labirinto[linha][coluna] == 'D') // verifica se é saída
+					saida = true;
+				else {
+					_labirinto[linha][coluna] = '.';
+					this.imprime();
+
+					if (!saida) {
+						saida = temSaida(linha + 1, coluna); // baixo
+					}
+
+					if (!saida) {
+						saida = temSaida(linha, coluna + 1); // direita
+					}
+
+					if (!saida) {
+						saida = temSaida(linha, coluna - 1); // esquerda
+					}
+
+					if (!saida) {
+						saida = temSaida(linha - 1, coluna); // cima
+					}
+				}
+			}
+		}
+
+		return saida;
 	}
 }
--- src/PrincipalLabirinto.java
+++ src/PrincipalLabirinto.java
@@ -1,15 +1,15 @@
 
 /*
- Crie uma classe chamada PrincipalLabirinto. 
+ OK - Crie uma classe chamada PrincipalLabirinto. 
  Nela, crie o método main. 
- 	No main, faça o trecho de código necessário para executar os métodos criados, 
+ 	OK No main, faça o trecho de código necessário para executar os métodos criados, 
  	percorrendo um determinado labirinto 
- 		(o nome do arquivo do labirinto deve ser indicado pelo usuário via teclado). 
- Ao final do main, deve ser criado um arquivo chamado "saidaLabirinto.txt" 
- 	com a frase: 
- 		"Existe um caminho para o labirinto" 
- 		ou "Não existe um caminho para o labirinto", 
-	de acordo com o resultado do método recursivo criado. 
+ 		OK - (o nome do arquivo do labirinto deve ser indicado pelo usuário via teclado). 
+ OK - Ao final do main, deve ser criado um arquivo chamado "saidaLabirinto.txt" 
+ 	OK - com a frase: 
+ 		OK - "Existe um caminho para o labirinto" 
+ 		OK - ou "Não existe um caminho para o labirinto", 
+	OK - de acordo com o resultado do método recursivo criado. 
  */
 import java.io.BufferedReader;
 import java.io.File;
@@ -24,93 +24,127 @@ import java.util.Date;
 import java.util.Scanner;
 
 public class PrincipalLabirinto {
-	private int linhaMax;
-	private int colunaMax;
-	private static String resultado;
+	private static int _linhaMax = 0;
+	private static int _colunaMax = 0;
+	private static String _resultado;
 	public static Scanner _scanner;
 
 	public static void main(String[] args) {
 		_scanner = new Scanner(System.in);
-		boolean arquivoEncontrado = false;
+		String filename = null;
 		File f;
 
+		boolean fimDeJogo = false;
+
 		do {
-			System.out.println("Digite o nome do arquivo:");
-			String filename = _scanner.nextLine();
+			System.out.println("Digite SAIR, para finalizar o programa. Se quiser continuar  o nome do arquivo:");
+			String comando = _scanner.nextLine();
 
-			f = new File(filename);
-			arquivoEncontrado = f.exists() ? true : false;
+			if (comando != null) {
+				if (comando.trim().equalsIgnoreCase("SAIR")) {
 
-		} while (!arquivoEncontrado);
+					fimDeJogo = true;
+				} else {
+					f = new File(comando);
 
-		if (arquivoEncontrado) {
-			System.out.println("Arquivo: " + f.getName());
-			System.out.println("Caminho: " + f.getAbsolutePath());
-			
-			Labirinto labirinto = new Labirinto(carregaLabirinto(""));
+					if (f.exists()) {
+						filename = f.getName();
+
+						System.out.println("++++++++++++++++++++");
+						System.out.println("+ ARQUIVO");
+						System.out.println("++++++++++++++++++++");
+						System.out.println("Nome: " + f.getName());
+						System.out.println("Caminho: " + f.getAbsolutePath());
+					}
+				}
+			}
+		} while (!fimDeJogo && (filename == null));
+
+		if (filename != null) {
+			Labirinto labirinto = new Labirinto(carregaLabirinto(filename));
 			labirinto.imprime();
 
-			if (labirinto.temSaida()) {
-				resultado = "Existe um caminho para o labirinto";
+			if (labirinto.temSaida(0, 0)) {
+				_resultado = "Existe um caminho para o labirinto";
 			} else {
-				resultado = "Não existe um caminho para o labirinto";
+				_resultado = "Não existe um caminho para o labirinto";
 			}
-			
-			System.out.println(resultado);
-		}
 
-		/*
-		 * String nomeDir = "../"; File f1 = new File(nomeDir); if (f1.isDirectory()) {
-		 * System.out.println("Diretório " + nomeDir); String s[] = f1.list(); for (int
-		 * i = 0; i < s.length; i++) { f = new File(s[i]); System.out.print(s[i]); if
-		 * (f.isDirectory()) System.out.println(" <dir> "); else
-		 * System.out.println(" <file>"); } } else System.out.println(nomeDir +
-		 * " não é um diretório.");
-		 * 
-		 * System.out.println("*********"); System.out.println("*********");
-		 * 
-		 * String texto = "Grêmio--é--o--melhor--time--!!"; String separada[] =
-		 * texto.split("--"); for (int i = 0; i < separada.length; i++) {
-		 * System.out.println("Palavra " + (i + 1) + ": " + separada[i]); }
-		 * 
-		 * System.out.println("*********");
-		 * 
-		 * try { f = new File("hino.txt"); FileWriter fr = new FileWriter(f);
-		 * PrintWriter out = new PrintWriter(fr); out.println("Até a pé nós iremos...");
-		 * out.print("Para o que der e vier...");
-		 * out.println("Mas o certo é que nós estaremos...");
-		 * out.println("Com o Grêmio onde o Grêmio estiver!"); out.close(); } catch
-		 * (IOException e) { System.out.println("Erro ao escrever arquivo."); }
-		 * 
-		 * System.out.println("*********");
-		 * 
-		 * filename = "descricao.txt";
-		 * 
-		 * try { FileReader fr = new FileReader(filename); BufferedReader in = new
-		 * BufferedReader(fr); String line = in.readLine(); while (line != null) {
-		 * System.out.println(line); line = in.readLine(); } in.close(); } catch
-		 * (FileNotFoundException e) { System.out.println("Arquivo \"" + filename +
-		 * "\" não existe."); } catch (IOException e) {
-		 * System.out.println("Erro na leitura de " + filename + "."); }
-		 */
+			System.out.println("++++++++++++++++++++");
+			System.out.println("+ RESULTADO");
+			System.out.println("++++++++++++++++++++");
+			System.out.println(_resultado);
+
+			try {
+				f = new File("saidaLabirinto.txt");
+				FileWriter fr = new FileWriter(f);
+				PrintWriter out = new PrintWriter(fr);
+				out.print(_resultado);
+				out.close();
+			} catch (IOException e) {
+				System.out.println("Erro: " + e.getMessage());
+			}
+		}
 	}
 
 	/*
-	 * Criar um método chamado "carregaLabirinto", que recebe por parâmetro uma
-	 * String fileName e retorna um array bidimensional de caracteres. Este método
-	 * abre o arquivo indicado por fileName e retorna um array de caracteres,
-	 * representado no arquivo.
-	 * 
-	 * A primeira linha do arquivo (vamos chamar de L) indica o número de linhas do
-	 * array e a segunda linha (vamos chamar de C) indica o número de colunas. Em
-	 * seguida, existem L linhas, cada uma com C colunas, preenchidas de forma a
-	 * criar o array do labirinto. O método cria um array bidimensional de
-	 * caracteres com as informações do arquivo e retorna este array. Dica: pesquise
-	 * e utilize o método charAt da classe String para percorrer todos os caracteres
-	 * de uma linha.
+	 OK - Criar um método chamado "carregaLabirinto", que recebe por parâmetro uma
+	 OK - String fileName e retorna um array bidimensional de caracteres. 
+	 Este método 
+	 	OK - abre o arquivo indicado por fileName e retorna um array de caracteres, 
+	 	OK - representado no arquivo. 
+	 	OK - A primeira linha do arquivo (vamos chamar de L) indica o número de linhas do array e 
+	 	OK - a segunda linha(vamos chamar de C) indica o número de colunas. 
+	 	OK - Em seguida, existem L linhas, cada uma com C colunas, preenchidas de forma a criar o array do labirinto. 
+	 	OK - O método cria um array bidimensional de caracteres com as informações do arquivo 
+	 	OK - e retorna este array. 
+	 	OK - Dica: pesquise e utilize o método charAt da classe String para percorrer todos os caracteres de uma linha.
 	 */
 	public static char[][] carregaLabirinto(String fileName) {
-		char[][] labirinto = new char[0][0];
+		char[][] labirinto = null;
+
+		try {
+			FileReader fr = new FileReader(fileName);
+			BufferedReader in = new BufferedReader(fr);
+			String line = in.readLine();
+			int l = 1;
+
+			System.out.println("++++++++++++++++++++");
+			System.out.println("+ CONTEÚDO");
+			System.out.println("++++++++++++++++++++");
+			while (line != null) {
+				System.out.println(line);
+
+				if (!line.trim().isEmpty()) {
+					try {
+						_linhaMax = (_linhaMax == 0 && l == 1) ? Integer.parseInt(line.trim()) : _linhaMax;
+						_colunaMax = (_colunaMax == 0 && l == 2) ? Integer.parseInt(line.trim()) : _colunaMax;
+					} catch (Exception e) {
+
+					}
+				}
+
+				if (_linhaMax > 0 && _colunaMax > 0 && labirinto == null) {
+					labirinto = new char[_linhaMax][_colunaMax];
+				}
+
+				if (l > 2 && l <= (_linhaMax + 2)) {
+					for (int i = 0; i <= _colunaMax - 1; i++) {
+						labirinto[l - 3][i] = line.charAt(i);
+					}
+				}
+
+				l++;
+
+				line = in.readLine(); // Próxima linha
+			}
+
+			in.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("Erro: Arquivo não encontrado. Msg:" + e.getMessage());
+		} catch (IOException e) {
+			System.out.println("Erro: Arquivo trancado." + e.getMessage());
+		}
 
 		return labirinto;
 	}
